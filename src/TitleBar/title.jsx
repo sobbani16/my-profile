5fe19934eda7bb424c69b4de6ea48ad881f9b239
Prashanth Sobbani <sobbani.prashanth@gmail.com>
2019-08-08 15:53:28 -0400

    Added react bootstrap

--- src/TitleBar/title.jsx
+++ src/TitleBar/title.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './title.css';
+import * as Button from '../../node_modules'
 
 const titleImageStyle = {
     height: '50%',
@@ -11,6 +12,7 @@ export default class Title extends React.Component {
         return (
             <div>
                 <p font-family="candyinc">Hi, this is</p>
+                <Button bsStyle="primary" bsSize="large" block>Hello New Button</Button>
 
                 <img style = {titleImageStyle} src={require('./prashanth.png')} alt="title" className="author-name" />
             </div>
