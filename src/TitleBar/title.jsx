4ce10afe376e6e3b49f56b2f69607915931b539a
Prashanth Sobbani <sobbani.prashanth@gmail.com>
2019-08-08 15:18:45 -0400

    changed the styles for the title

--- src/TitleBar/title.jsx
+++ src/TitleBar/title.jsx
@@ -10,7 +10,7 @@ export default class Title extends React.Component {
     render() {
         return (
             <div>
-                <h1 font-family="candyinc">Hi, this is</h1>
+                <p font-family="candyinc">Hi, this is</p>
 
                 <img style = {titleImageStyle} src={require('./prashanth.png')} alt="title" className="author-name" />
             </div>
