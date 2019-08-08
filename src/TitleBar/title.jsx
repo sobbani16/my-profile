e647e3d9fbdba57a3e5e9ad4f8f3df0b69dd3813
Prashanth Sobbani <sobbani.prashanth@gmail.com>
2019-08-08 16:38:34 -0400

    Added a button

--- src/TitleBar/title.jsx
+++ src/TitleBar/title.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './title.css';
-import * as Button from '../../node_modules'
+import Button from '../../node_modules/react-bootstrap/Button';
 
 const titleImageStyle = {
     height: '50%',
     width: '50%'
+}
+
+const titleButtonStyle = {
+    maxWidth: 100, margin:'0 auto 10px'
 }
 
 export default class Title extends React.Component {
@@ -12,9 +16,10 @@ export default class Title extends React.Component {
         return (
             <div>
                 <p font-family="candyinc">Hi, this is</p>
-                <Button bsStyle="primary" bsSize="large" block>Hello New Button</Button>
+                
 
                 <img style = {titleImageStyle} src={require('./prashanth.png')} alt="title" className="author-name" />
+                <Button style = {titleButtonStyle} bsStyle="primary" bsSize="large" block>Click Here !!!</Button>
             </div>
         )
     }
